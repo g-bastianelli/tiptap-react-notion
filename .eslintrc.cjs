c032cb2972d7e3a7330df7a4b3f8aa09a707e4c1
gbastianelli <guillaume.bastianelli@gmail.com>
2022-10-19 14:43:23 +0200

    eslint + prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    'standard-with-typescript',
+    'prettier'
+  ],
+  overrides: [
+  {
+    // disable the rule specifically for tsx files
+    "files": ["*.tsx"],
+    "rules": {
+      "@typescript-eslint/explicit-function-return-type": "off"
+    }
+  }
+  ],
+  "parser": "@typescript-eslint/parser",
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    "@typescript-eslint",
+    'react'
+  ],
+  rules: {
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import tiptapLogo from './assets/tiptap.svg'
-import Editor from './Editor';
-import './App.css'
+import React from "react";
+import reactLogo from "./assets/react.svg";
+import tiptapLogo from "./assets/tiptap.svg";
+import { DemoEditor } from "./Editor";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/public/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-          <a href="https://tiptap.dev" target="_blank">
-              <img src={tiptapLogo} className="logo tiptap" alt="Tiptap logo" />
-          </a>
+        <a href="https://tiptap.dev" target="_blank" rel="noreferrer">
+          <img src={tiptapLogo} className="logo tiptap" alt="Tiptap logo" />
+        </a>
       </div>
       <h1>Vite + React + Tiptap</h1>
       <div className="card">
-        <Editor/>
+        <DemoEditor />
       </div>
       <p className="read-the-docs">
         Click on the Vite React and Tiptap logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
