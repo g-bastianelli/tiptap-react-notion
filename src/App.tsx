bf29282754e126273314630b48e2beeca9fa5b1b
gbastianelli <guillaume.bastianelli@gmail.com>
2022-10-19 12:40:16 +0200

    setup tiptap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import tiptapLogo from './assets/tiptap.svg'
+import Editor from './Editor';
 import './App.css'
 
 function App() {
@@ -14,18 +16,16 @@ function App() {
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+          <a href="https://tiptap.dev" target="_blank">
+              <img src={tiptapLogo} className="logo tiptap" alt="Tiptap logo" />
+          </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Tiptap</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Editor/>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite React and Tiptap logos to learn more
       </p>
     </div>
   )
